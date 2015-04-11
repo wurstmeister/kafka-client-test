c1226a01f2da32061ccb48a599183fb0e50bca7a
Wurstmeister <wurstmeister@users.noreply.github.com>
2015-04-11 14:08:12 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,91 @@
 # kafka-client-test
+
+Temporary test project for validating kafka 0.8.3 client for apache storm
+
+Dependencies: 
+
+- docker compose 
+- boot2docker (if you run docker natively update KAFKA_ADVERTISED_HOST_NAME and KAFKA_ZOOKEEPER_CONNECT properties
+- https://raw.githubusercontent.com/wurstmeister/kafka-docker/master/start-kafka-shell.sh
+- Kafka dev image built with: https://github.com/wurstmeister/kafka-docker/blob/dev/Dockerfile (available on docker hub)
+
+
+Common setup for tests:
+
+```docker-compose -f zk.yml up -d```
+
+```docker-compose -f broker-1.yml up -d```
+
+```docker-compose -f broker-2.yml up -d```
+
+
+```start-kafka-shell.sh 192.168.59.103 192.168.59.103:49181```
+
+```$KAFKA_HOME/bin/kafka-topics.sh --create --topic topic --partitions 2 --zookeeper $ZK --replication-factor 2```
+
+```$KAFKA_HOME/bin/kafka-topics.sh --describe --topic topic --zookeeper $ZK```
+
+Test cleanup:
+
+```docker-compose stop```
+
+```docker-compose rm```
+
+
+Test case 1 (Per topic subscription):
+
+```java -jar target/kafka-client-test-1.0-SNAPSHOT-jar-with-dependencies.jar TOPIC```
+
+
+Kill Broker 1:
+
+```docker-compose -f broker-1.yml stop kafka```
+
+New leader will be identified 
+
+Restart Broker 1
+
+```docker-compose -f broker-1.yml up -d```
+
+Kill Broker 2: 
+
+```docker-compose -f broker-2.yml stop kafka2```
+
+New leader will be identified
+
+Restart Broker 2:
+
+```docker-compose -f broker-2.yml up -d```
+
+Kill Broker 1:
+
+```docker-compose -f broker-1.yml stop kafka```
+
+New leader will be identified
+
+--> OK
+
+
+Test case 2 (per partition subscription):
+
+```java -jar target/kafka-client-test-1.0-SNAPSHOT-jar-with-dependencies.jar PER_PARTITION```
+
+Kill Broker 1:
+
+```docker-compose -f broker-1.yml stop kafka```
+
+New leader will be identified 
+
+Restart Broker 1
+
+```docker-compose -f broker-1.yml up -d```
+
+Kill Broker 2: 
+
+```docker-compose -f broker-2.yml stop kafka2```
+
+--> New leader is not identified
+
+
+
+
---
+++ src/main/java/org/wurstmeister/TestCase.java
@@ -0,0 +1,58 @@
+package org.wurstmeister;
+
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.TopicPartition;
+
+import java.util.List;
+import java.util.Properties;
+
+
+public class TestCase {
+
+    private static enum Mode {
+        PER_PARTITION,
+        TOPIC
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Mode mode = Mode.valueOf(args[0]);
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "192.168.59.103:49093,192.168.59.103:49094");
+        props.put("group.id", "my-test-2");
+        props.put("enable.auto.commit", "false");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+        String topic = "topic";
+
+        List<PartitionInfo> partitionInfos = consumer.partitionsFor("topic");
+
+        if (mode.equals(Mode.PER_PARTITION) ) {
+            for (int i = 0; i < partitionInfos.size(); i++) {
+                TopicPartition partition = new TopicPartition(topic, i);
+                consumer.subscribe(partition);
+            }
+        } else {
+            consumer.subscribe(topic);
+        }
+
+        System.out.println(consumer.subscriptions());
+
+        while (true) {
+            System.out.println("Checking partitions ---------------");
+            consumer.poll(100);
+            partitionInfos = consumer.partitionsFor("topic");
+            for (PartitionInfo partitionInfo : partitionInfos) {
+                System.out.println(partitionInfo + " leader " + partitionInfo.leader());
+            }
+            Thread.sleep(2000);
+        }
+    }
+}
